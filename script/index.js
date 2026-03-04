788ee4918db8372f07d720450be9111ccd99dc08
NizamBappy <nizamcse66@gmail.com>
2026-03-04 15:48:59 +0300

    add loading spinner

--- script/index.js
+++ script/index.js
@@ -1,5 +1,21 @@
 // console.log('File Connected')
 
+const createElements = (arr) =>{
+    const htmlElements = arr.map((el) =>`<span class="btn">${el}</span>`)
+    return(htmlElements.join(" "))
+}
+
+const manageSpinner = (status) =>{
+    if(status== true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("word-container").classList.add("hidden");
+    }else{
+        document.getElementById("word-container").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+        
+    }
+}
+
  const loadLesson = () =>{
     const url = "https://openapi.programming-hero.com/api/levels/all";
 
@@ -15,6 +31,7 @@
  };
  const loadLevelWord = (id) =>{
     // console.log(id);
+    manageSpinner(true);
       const url = `https://openapi.programming-hero.com/api/level/${id}`;
 
     //   console.log(url);
@@ -29,6 +46,38 @@
             displayWord(json.data)
       });
  };
+ const loadWordDetail = async(id) =>{
+    const url = `https:openapi.programming-hero.com/api/word/${id}`;
+    // console.log(url);
+    const res = await fetch(url)
+    const details = await res.json();
+    displayWordDetails(details.data);
+ };
+//  {word: 'Eager', meaning: 'আগ্রহী', pronunciation: 'ইগার', level: 1, sentence: 'The kids were eager to open their gifts.', …}
+
+ const displayWordDetails = (word) =>{
+    console.log(word);
+    const detailsModal = document.getElementById("details-container");
+    detailsModal.innerHTML=`<div>
+            <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone"></i>:
+            ${word.pronunciation})</h2>
+        </div>
+        <div class="space-y-2">
+            <h2 class="font-bold">Meaning</h2>
+            <p>${word.meaning?word.meaning:"অর্থ পাওয়া জায় নি"}</p>
+        </div>
+        <div class="space-y-2">
+            <h2 class="font-bold">Example</h2>
+            <p>${word.sentence}</p>
+        </div>
+        <div class="space-y-2">
+            <h2 class="font-bangla font-bold">সমার্থক শব্দ গুলো</h2>
+            <div class"">
+                ${createElements(word.synonyms)}
+            </div>
+        </div>`;
+    document.getElementById("word_modal").showModal();
+ }
  
  const displayWord = (words) =>{
     // console.log(words);
@@ -41,6 +90,7 @@
             <h2 class="font-bangla text-4xl font font-bold">নেক্সট Lesson এ যান</h2>
         
         </div>`;
+        manageSpinner(false);
         return;
     }
     words.forEach(word=>{
@@ -52,13 +102,14 @@
             <p class="text-xl font-medium">Meaning / Pronounciation</p>
             <div class="font-bangla text-2xl font-semibold">"${word.meaning? word.meaning:"অর্থ পাওয়া জায় নি"} / ${word.pronunciation? word.pronunciation:"Pronounciation পাওয়া জায় নি"}"</div>
             <div class="flex justify-between items-center">
-            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+            <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
             <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
         </div>`
 
         wordContainer.appendChild(card);
     });
+    manageSpinner(false);
  };
 
  const displayLesson = (lessons) =>{
